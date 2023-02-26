3606503f92bf861a78c1caa520cdabafe9629372
Luna <swagnemite@gmx.de>
2023-02-26 19:02:45 +0100

    add debug hud

--- lua/entities/lvs_base_wheeldrive/cl_init.lua
+++ lua/entities/lvs_base_wheeldrive/cl_init.lua
@@ -9,3 +9,42 @@ include("sh_collisionfilter.lua")
 
 	return LVS:CalcView( self, ply, pos, angles, fov, pod )
 end
+
+ENT.IconEngine = Material( "lvs/engine.png" )
+
+function ENT:LVSHudPaintInfoText( X, Y, W, H, ScrX, ScrY, ply )
+	local kmh = math.Round(self:GetVelocity():Length() * 0.09144,0)
+	draw.DrawText( "km/h ", "LVS_FONT", X + 72, Y + 35, color_white, TEXT_ALIGN_RIGHT )
+	draw.DrawText( kmh, "LVS_FONT_HUD_LARGE", X + 72, Y + 20, color_white, TEXT_ALIGN_LEFT )
+
+	if ply ~= self:GetDriver() then return end
+
+	local Throttle = self:GetThrottle()
+	local Col = Throttle <= 1 and color_white or Color(0,0,0,255)
+	local hX = X + W - H * 0.5
+	local hY = Y + H * 0.25 + H * 0.25
+
+	surface.SetMaterial( self.IconEngine )
+	surface.SetDrawColor( 0, 0, 0, 200 )
+	surface.DrawTexturedRectRotated( hX + 4, hY + 1, H * 0.5, H * 0.5, 0 )
+	surface.SetDrawColor( color_white )
+	surface.DrawTexturedRectRotated( hX + 2, hY - 1, H * 0.5, H * 0.5, 0 )
+
+	if not self:GetEngineActive() then
+		draw.SimpleText( "X" , "LVS_FONT",  hX, hY, Color(0,0,0,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+	end
+
+	self:LVSDrawCircle( hX, hY, H * 0.35, math.min( Throttle, 1 ) )
+
+	if Throttle > 1 then
+		draw.SimpleText( "+"..math.Round((Throttle - 1) * 100,0).."%" , "LVS_FONT",  hX, hY, Col, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+	end
+end
+
+function ENT:LVSPreHudPaint( X, Y, ply )
+	return true
+end
+
+function ENT:LVSHudPaint( X, Y, ply )
+	if not self:LVSPreHudPaint( X, Y, ply ) then return end
+end
