9db0df2862d78a6655fd0b04095c89a6db6ef771
Luna <swagnemite@gmx.de>
2023-04-10 15:17:36 +0200

    testscript

--- lua/autorun/lvs_cars_base.lua
+++ lua/autorun/lvs_cars_base.lua
@@ -1,11 +1,11 @@
 
+local lvsCars = {}
+
 hook.Add( "PreRegisterSENT", "!!!!!!lvs_register_simfphys_vehicles", function( ent, class )
 	if class ~= "lvs_base_wheeldrive" then return end
 
 	for name, data in pairs( list.Get( "simfphys_vehicles" ) ) do
-		--print( name )
-		--print( data.Name )
-		--print( data.Members )
+		if not istable( data.Members ) then continue end
 
 		local ENT = {}
 
@@ -20,6 +20,16 @@ hook.Add( "PreRegisterSENT", "!!!!!!lvs_register_simfphys_vehicles", function( e
 		ENT.Spawnable		= data.Spawnable ~= false
 		ENT.AdminSpawnable = data.AdminSpawnable == true
 
+		lvsCars[ name ] = true
+
 		scripted_ents.Register( ENT, name )
 	end
+end )
+
+hook.Add( "OnEntityCreated", "!!!!lvs_just_in_time_table_merge", function( ent )
+	local class = ent:GetClass()
+
+	if not lvsCars[ class ] then return end
+
+	table.Merge( ent, list.Get( "simfphys_vehicles" )[ class ].Members )
 end )
