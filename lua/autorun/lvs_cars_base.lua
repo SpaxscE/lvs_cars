3bc15416a0d8e431cf392c1a39d932176777d5c5
Luna <swagnemite@gmx.de>
2023-04-10 18:12:35 +0200

    4wheel_drive => base_wheeldrive

--- lua/autorun/lvs_cars_base.lua
+++ lua/autorun/lvs_cars_base.lua
@@ -9,7 +9,7 @@ hook.Add( "PreRegisterSENT", "!!!!!!lvs_register_simfphys_vehicles", function( e
 
 		local ENT = {}
 
-		ENT.Base = "lvs_4wheel_drive"
+		ENT.Base = "lvs_base_wheeldrive"
 
 		ENT.MDL = data.Model
 
@@ -27,9 +27,19 @@ hook.Add( "PreRegisterSENT", "!!!!!!lvs_register_simfphys_vehicles", function( e
 end )
 
 hook.Add( "OnEntityCreated", "!!!!lvs_just_in_time_table_merge", function( ent )
+	if not IsValid( ent ) then return end
+
 	local class = ent:GetClass()
 
 	if not lvsCars[ class ] then return end
 
-	table.Merge( ent, list.Get( "simfphys_vehicles" )[ class ].Members )
+	if SERVER then
+		ent:InitFromList( class )
+	else
+		timer.Simple( 0, function()
+			if not IsValid( ent ) then return end
+
+			ent:InitFromList( class )
+		end )
+	end
 end )
