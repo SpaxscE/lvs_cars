688731e8a2f4d8c6a852b52ec433ba8315781d31
Diggergithub <88580941+Diggergithub@users.noreply.github.com>
2023-07-28 04:48:21 +0500

    added highwayman
    added to try github
    delete stuff if not needed

---
+++ lua/entities/lvs_wheeldrive_highwayman/init.lua
@@ -0,0 +1,96 @@
+AddCSLuaFile( "shared.lua" )
+AddCSLuaFile( "cl_init.lua" )
+include("shared.lua")
+
+function ENT:OnSpawn( PObj )
+	PObj:SetMass( 1000 )
+	PObj:EnableDrag( false )
+
+	self:AddDriverSeat( Vector(-10,16,-1), Angle(0,-90,10) )
+	self:AddPassengerSeat( Vector(0,-16,8), Angle(0,-90,20) )
+
+	self:AddEngine( Vector(45,0,20) )
+
+	local WheelModel = "models/DiggerCars/highwayman/wheel.mdl"
+
+	local FrontAxle = self:DefineAxle( {
+		Axle = {
+			ForwardAngle = Angle(0,0,0),
+			SteerType = LVS.WHEEL_STEER_FRONT,
+			SteerAngle = 30,
+			TorqueFactor = 0,
+			BrakeFactor = 1,
+		},
+		Wheels = {
+			self:AddWheel( {
+				pos = Vector(59.1,32,10),
+
+				mdl = WheelModel,
+				mdl_ang = Angle(-90,180,90),
+
+				camber = -0.5,
+				caster = 5,
+				toe = 0,
+			} ),
+
+			self:AddWheel( {
+				pos = Vector(59.1,-32,10),
+
+				mdl = WheelModel,
+				mdl_ang = Angle(-90,0,90),
+
+				camber = -0.5,
+				caster = 5,
+				toe = 0,
+			} ),
+		},
+		Suspension = {
+			Height = 10,
+			MaxTravel = 7,
+			ControlArmLength = 25,
+			SpringConstant = 20000,
+			SpringDamping = 2000,
+			SpringRelativeDamping = 2000,
+		},
+	} )
+
+	local RearAxle = self:DefineAxle( {
+		Axle = {
+			ForwardAngle = Angle(0,0,0),
+			SteerType = LVS.WHEEL_STEER_NONE,
+			TorqueFactor = 1,
+			BrakeFactor = 1,
+		},
+		Wheels = {
+			self:AddWheel( {
+				pos = Vector(-59.5,32,8),
+
+				mdl = WheelModel,
+				mdl_ang = Angle(-90,180,90),
+			} ),
+
+			self:AddWheel( {
+				pos = Vector(-59.5,-32,8),
+
+				mdl = WheelModel,
+				mdl_ang = Angle(-90,0,90),
+			} ),
+		},
+		Suspension = {
+			Height = 10,
+			MaxTravel = 7,
+			ControlArmLength = 25,
+			SpringConstant = 20000,
+			SpringDamping = 2000,
+			SpringRelativeDamping = 2000,
+		},
+	} )
+end
+
+function ENT:OnEngineActiveChanged( Active )
+	if Active then
+		self:EmitSound( "lvs/vehicles/highwayman/CARSTART.wav" )
+	else
+		self:EmitSound( "lvs/vehicles/kuebelwagen/engine_stop.wav" )
+	end
+end
---
+++ lua/entities/lvs_wheeldrive_highwayman/shared.lua
@@ -0,0 +1,12 @@
+
+ENT.Base = "lvs_base_wheeldrive"
+
+ENT.PrintName = "Chryslus Highwayman"
+ENT.Author = "Digger"
+ENT.Information = "Luna's Vehicle Script"
+ENT.Category = "[LVS] - Cars"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+ENT.MDL = "models/DiggerCars/highwayman/1.mdl"
