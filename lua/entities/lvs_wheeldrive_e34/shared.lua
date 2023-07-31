527b357dcbe62db5f112fd15ee0a1267b5c87bbc
Luna <swagnemite@gmx.de>
2023-08-01 00:37:00 +0200

    bmw adjustments

--- lua/entities/lvs_wheeldrive_e34/shared.lua
+++ lua/entities/lvs_wheeldrive_e34/shared.lua
@@ -10,3 +10,11 @@ ENT.Spawnable			= true
 ENT.AdminSpawnable		= false
 
 ENT.MDL = "models/diggercars/BMW_M5E34/e34.mdl"
+
+ENT.MaxVelocity = 1600
+
+ENT.EnginePower = 500
+ENT.EngineTorque = 200
+
+ENT.WheelDownForce = 500
+ENT.WheelDownForcePowered = 2000
