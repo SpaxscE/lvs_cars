5c73d753b6388877816fff3372306abd960bac64
Luna <swagnemite@gmx.de>
2023-08-02 19:51:28 +0200

    ?highwayman sounds?

--- lua/entities/lvs_wheeldrive_highwayman/shared.lua
+++ lua/entities/lvs_wheeldrive_highwayman/shared.lua
@@ -10,3 +10,36 @@ ENT.Spawnable			= true
 ENT.AdminSpawnable		= false
 
 ENT.MDL = "models/diggercars/highwayman/1.mdl"
+
+ENT.MaxVelocity = 1200
+
+ENT.EnginePower = 25
+ENT.EngineTorque = 350
+
+ENT.TransGears = 4
+ENT.TransMinGearHoldTime = 1
+ENT.TransShiftSpeed = 0.3
+
+ENT.WheelDownForce = 1000
+ENT.WheelDownForcePowered = 1000
+
+ENT.EngineSounds = {
+	{
+		sound = "lvs/vehicles/kuebelwagen/engine_low.wav",
+		Volume = 0.5,
+		Pitch = 85,
+		PitchMul = 25,
+		Type = 0,
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+	{
+		sound = "lvs/vehicles/kuebelwagen/engine_mid.wav",
+		Volume = 1,
+		Pitch = 100,
+		PitchMul = 100,
+		Type = 1,
+		SoundLevel = 75,
+		UseDoppler = true,
+	},
+}
