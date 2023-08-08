3d8ac9d7a9b1355b598f32e36b2263e91800511d
Luna <swagnemite@gmx.de>
2023-08-08 14:51:23 +0200

    effect adjustments

--- lua/effects/lvs_physics_wheelwatersplash.lua
+++ lua/effects/lvs_physics_wheelwatersplash.lua
@@ -1,39 +1,42 @@
 
+EFFECT.WaterWake = Material("effects/splashwake1")
+
 function EFFECT:Init( data )
-	local Pos = data:GetOrigin()
+	local Pos = data:GetOrigin() + Vector(0,0,1)
 	local Ent = data:GetEntity()
 	self.Size = data:GetMagnitude()
 
 	if not IsValid( Ent ) then return end
 
-	self.LifeTime = math.Rand(1.5,3)
+	self.LifeTime = 1
 	self.DieTime = CurTime() + self.LifeTime
 
 	self.Splash = {
 		Pos = Pos,
-		Mat = Material("effects/splashwake1"),
+		Mat = self.WaterWake,
 		RandomAng = math.random(0,360),
 	}
 
 	local emitter = Ent:GetParticleEmitter( Ent:GetPos() )
+	local Vel = Ent:GetVelocity():Length()
 
-	if emitter and emitter.Add then
-		local particle = emitter:Add( "effects/splash4", Pos )
-		if not particle then return end
+	for i = 1, 3 do
+		if emitter and emitter.Add then
+			local particle = emitter:Add( "effects/splash4", Pos + VectorRand() * self.Size * 0.1 )
+			if not particle then continue end
 
-		local Vel = Ent:GetVelocity():Length()
-
-		particle:SetVelocity( Vector(0,0,math.Clamp(Vel / 2,100,250)) )
-		particle:SetDieTime( 0.25 + math.min(Vel / 200,0.35) )
-		particle:SetAirResistance( 60 ) 
-		particle:SetStartAlpha( 150 )
-		particle:SetEndAlpha( 0 )
-		particle:SetStartSize( self.Size * 0.2 )
-		particle:SetEndSize(  self.Size * 2 )
-		particle:SetRoll( math.Rand(-1,1) * 100 )
-		particle:SetColor( 255,255,255 )
-		particle:SetGravity( Vector( 0, 0, -600 ) )
-		particle:SetCollide( false )
+			particle:SetVelocity( Vector(0,0,math.Clamp(Vel / 100,100,250)) )
+			particle:SetDieTime( 0.25 + math.min(Vel / 500,0.2) )
+			particle:SetAirResistance( 60 ) 
+			particle:SetStartAlpha( 150 )
+			particle:SetEndAlpha( 0 )
+			particle:SetStartSize( self.Size * 0.2 )
+			particle:SetEndSize(  self.Size )
+			particle:SetRollDelta( math.Rand(-1,1) * 5 )
+			particle:SetColor( 255,255,255 )
+			particle:SetGravity( Vector( 0, 0, -600 ) )
+			particle:SetCollide( false )
+		end
 	end
 end
 
@@ -47,14 +50,18 @@ end
 
 function EFFECT:Render()
 	if self.Splash and self.LifeTime then
-		local Scale = ((self.DieTime - self.LifeTime - CurTime()) / self.LifeTime)
-		local S =  self.Size * 2.5 + (self.Size * 2.5) * Scale
-		local Alpha = 100 + 100 * Scale
+		local Scale = 1 - (self.DieTime - CurTime()) / self.LifeTime
 
-		cam.Start3D2D( self.Splash.Pos + Vector(0,0,1), Angle(0,0,0), 1 )
+		local Alpha = math.max( 100 - 150 * Scale ^ 2, 0 )
+
+		if Alpha <= 0 then return end
+
+		local Size = (self.Size + self.Size * Scale) * 1.5
+
+		cam.Start3D2D( self.Splash.Pos, Angle(0,0,0), 1 )
 			surface.SetMaterial( self.Splash.Mat )
 			surface.SetDrawColor( 255, 255, 255 , Alpha )
-			surface.DrawTexturedRectRotated( 0, 0, S , S, self.Splash.RandomAng )
+			surface.DrawTexturedRectRotated( 0, 0, Size, Size, self.Splash.RandomAng )
 		cam.End3D2D()
 	end
 end
