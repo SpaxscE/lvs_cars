e2907c70c4cb8cca8be97058e71238c2d32493fb
Luna <swagnemite@gmx.de>
2023-08-08 15:46:56 +0200

    lights test

--- lua/entities/lvs_wheeldrive_porsche930targa/cl_init.lua
+++ lua/entities/lvs_wheeldrive_porsche930targa/cl_init.lua
@@ -1,1 +1,32 @@
 include("shared.lua")
+
+function ENT:OnSpawn()
+	local SubMaterialID = 19
+
+	local Mat = self:GetMaterials()[ SubMaterialID + 1 ] 
+
+	local EntID = self:EntIndex()
+
+	local data = util.KeyValuesToTable( file.Read( "materials/"..Mat..".vmt", "GAME" ) )
+
+	self.TestMaterial = CreateMaterial("lights"..EntID, "VertexLitGeneric", data )
+
+	self.TestMaterial:SetFloat("$detailblendfactor", 0 )
+
+	self:SetSubMaterial( SubMaterialID, "!lights"..EntID )
+end
+
+function ENT:OnFrameActive()
+end
+
+function ENT:OnFrame()
+	if not self.TestMaterial then return end
+
+	self.TestMaterial:SetFloat("$detailblendfactor", math.max( math.cos( CurTime() * 2 ), 0 ) )
+end
+
+function ENT:OnEngineActiveChanged( Active )
+end
+
+function ENT:OnActiveChanged( Active )
+end
