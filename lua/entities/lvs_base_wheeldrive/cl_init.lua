5883ef51de42c9209cc4c8cc73b7fe717336a266
Luna <swagnemite@gmx.de>
2023-08-11 13:44:05 +0200

    move camera stuff to camera.lua

--- lua/entities/lvs_base_wheeldrive/cl_init.lua
+++ lua/entities/lvs_base_wheeldrive/cl_init.lua
@@ -2,6 +2,7 @@ include("shared.lua")
 include("sh_animations.lua")
 include("cl_flyby.lua")
 include("cl_tiresounds.lua")
+include("cl_camera.lua")
 
 DEFINE_BASECLASS( "lvs_base" )
 
@@ -42,45 +43,6 @@ function ENT:OnChangeGear( oldGear, newGear )
 	self:EmitSound( "buttons/lever7.wav", 75, 80, 0.25 )
 
 	self:SuppressViewPunch( self.TransShiftSpeed )
-end
-
-function ENT:SuppressViewPunch( time )
-	self._viewpunch_supressed_time = CurTime() + (time or 0.2)
-end
-
-function ENT:IsViewPunchSuppressed()
-	return (self._viewpunch_supressed_time or 0) > CurTime()
-end
-
-function ENT:CalcViewPunch( ply, pos, angles, fov, pod )
-	local Vel = self:GetVelocity()
-	local VelLength = Vel:Length()
-	local VelPercentMaxSpeed = math.min( VelLength / 1000, 1 )
-
-	if ply:lvsMouseAim() then
-		angles = ply:EyeAngles()
-	end
-
-	local direction = (90 - self:AngleBetweenNormal( angles:Forward(), Vel:GetNormalized() )) / 90
-
-	local FovValue = math.min( VelPercentMaxSpeed ^ 2 * 100, 15 )
-
-	local Throttle = self:GetThrottle()
-	local Brake = self:GetBrake()
-
-	if self:IsViewPunchSuppressed() then
-		self._viewpunch_fov = self._viewpunch_fov and self._viewpunch_fov + (-VelPercentMaxSpeed * FovValue - self._viewpunch_fov) * RealFrameTime() or 0
-	else
-		local newFov =(1 - VelPercentMaxSpeed) * Throttle * FovValue - VelPercentMaxSpeed * Brake * FovValue
-
-		self._viewpunch_fov = self._viewpunch_fov and self._viewpunch_fov + (newFov - self._viewpunch_fov) * RealFrameTime() * 10 or 0
-	end
-
-	if pod == self:GetDriverSeat() then
-		pos = pos + pod:GetUp() * 7 - pod:GetRight() * 11
-	end
-
-	return self._viewpunch_fov * (90 - self:AngleBetweenNormal( angles:Forward(), Vel:GetNormalized() )) / 90
 end
 
 ENT.IconEngine = Material( "lvs/engine.png" )
@@ -127,40 +89,4 @@ LVS:AddHudEditor( "CarMenu",  ScrW() - 690, ScrH() - 85,  220, 75, 220, 75, "CAR
 
 function ENT:LVSHudPaintCarMenu( X, Y, w, h, ScrX, ScrY, ply )
 	if not ply:lvsKeyDown( "CAR_MENU" ) then return end
-end
-
-
-function ENT:CalcViewMouseAim( ply, pos, angles, fov, pod )
-	angles = ply:EyeAngles()
-	return LVS:CalcView( self, ply, pos, angles, fov, pod )
-end
-
-function ENT:CalcViewOverride( ply, pos, angles, fov, pod )
-	return pos, angles, fov
-end
-
-function ENT:CalcViewDriver( ply, pos, angles, fov, pod )
-	pos = pos + pod:GetUp() * 7 - pod:GetRight() * 11
-
-	if ply:lvsMouseAim() then
-		return self:CalcViewMouseAim( ply, pos, angles, fov, pod )
-	else
-		return LVS:CalcView( self, ply, pos, angles,  fov, pod )
-	end
-end
-
-function ENT:CalcViewPassenger( ply, pos, angles, fov, pod )
-	return LVS:CalcView( self, ply, pos, angles, fov, pod )
-end
-
-function ENT:LVSCalcView( ply, original_pos, original_angles, original_fov, pod )
-	local pos, angles, fov = self:CalcViewOverride( ply, original_pos, original_angles, original_fov, pod )
-
-	local new_fov = math.min( fov + self:CalcViewPunch( ply, pos, angles, fov, pod ), 180 )
-
-	if self:GetDriverSeat() == pod then
-		return self:CalcViewDriver( ply, pos, angles, new_fov, pod )
-	else
-		return self:CalcViewPassenger( ply, pos, angles, new_fov, pod )
-	end
 end
