7796d7810df0ebfcaf6f896207d602579109afbd
Luna <swagnemite@gmx.de>
2023-08-13 09:59:03 +0200

    cleaned up lights table on w123

--- lua/entities/lvs_wheeldrive_w123/shared.lua
+++ lua/entities/lvs_wheeldrive_w123/shared.lua
@@ -55,127 +55,49 @@ ENT.Lights = {
 		Trigger = "main",
 		SubMaterialID = 24,
 		Sprites = {
-			[1] = {
-				pos = Vector(95.99,24.72,21.01),
-				colorB = 200,
-				colorA = 150,
-			},
-			[2] = {
-				pos = Vector(95.99,-24.72,21.01),
-				colorB = 200,
-				colorA = 150,
-			},
-			[3] = {
-				pos = Vector(-86.2,21.51,21.1),
-				colorG = 0,
-				colorB = 0,
-				colorA = 150,
-			},
-			[4] = {
-				pos = Vector(-86.2,-21.51,21.1),
-				colorG = 0,
-				colorB = 0,
-				colorA = 150,
-			},
+			{ pos = Vector(95.99,24.72,21.01), colorB = 200, colorA = 150 },
+			{ pos = Vector(95.99,-24.72,21.01), colorB = 200, colorA = 150 },
+			{ pos = Vector(-86.2,21.51,21.1), colorG = 0, colorB = 0, colorA = 150 },
+			{ pos = Vector(-86.2,-21.51,21.1), colorG = 0, colorB = 0, colorA = 150 },
 		},
 		ProjectedTextures = {
-			[1] = {
-				pos = Vector(95.99,24.72,21.01),
-				ang = Angle(0,0,0),
-				colorB = 200,
-				colorA = 150,
-				shadows = true,
-			},
-			[2] = {
-				pos = Vector(95.99,-24.72,21.01),
-				ang = Angle(0,0,0),
-				colorB = 200,
-				colorA = 150,
-				shadows = true,
-			},
+			{ pos = Vector(95.99,24.72,21.01), ang = Angle(0,0,0), colorB = 200, colorA = 150, shadows = true },
+			{ pos = Vector(95.99,-24.72,21.01), ang = Angle(0,0,0), colorB = 200, colorA = 150, shadows = true },
 		},
 	},
 	{
 		Trigger = "high",
 		Sprites = {
-			[1] = {
-				pos = Vector(95.99,24.72,21.01),
-				colorB = 200,
-				colorA = 150,
-			},
-			[2] = {
-				pos = Vector(95.99,-24.72,21.01),
-				colorB = 200,
-				colorA = 150,
-			},
+			{ pos = Vector(95.99,24.72,21.01), colorB = 200, colorA = 150 },
+			{ pos = Vector(95.99,-24.72,21.01), colorB = 200, colorA = 150 },
 		},
 		ProjectedTextures = {
-			[1] = {
-				pos = Vector(95.99,24.72,21.01),
-				ang = Angle(0,0,0),
-				colorB = 200,
-				colorA = 150,
-				shadows = true,
-			},
-			[2] = {
-				pos = Vector(95.99,-24.72,21.01),
-				ang = Angle(0,0,0),
-				colorB = 200,
-				colorA = 150,
-				shadows = true,
-			},
+			{ pos = Vector(95.99,24.72,21.01), ang = Angle(0,0,0), colorB = 200, colorA = 150, shadows = true },
+			{ pos = Vector(95.99,-24.72,21.01), ang = Angle(0,0,0), colorB = 200, colorA = 150, shadows = true },
 		},
 	},
 	{
 		Trigger = "brake",
 		SubMaterialID = 31,
 		Sprites = {
-			[1] = {
-				pos = Vector(-86.03,26.09,21.32),
-				colorG = 0,
-				colorB = 0,
-				colorA = 150,
-			},
-			[2] = {
-				pos = Vector(-86.03,-26.09,21.32),
-				colorG = 0,
-				colorB = 0,
-				colorA = 150,
-			},
+			{ pos = Vector(-86.03,26.09,21.32), colorG = 0, colorB = 0, colorA = 150 },
+			{ pos = Vector(-86.03,-26.09,21.32), colorG = 0, colorB = 0, colorA = 150 },
 		}
 	},
 	{
 		Trigger = "reverse",
 		SubMaterialID = 27,
 		Sprites = {
-			[1] = {
-				pos = Vector(-86,17.49,21.19),
-				height = 25,
-				width = 25,
-				colorA = 150,
-			},
-			[2] = {
-				pos = Vector(-86,-17.49,21.19),
-				height = 25,
-				width = 25,
-				colorA = 150,
-			},
+			{ pos = Vector(-86,17.49,21.19), height = 25, width = 25, colorA = 150 },
+			{ pos = Vector(-86,-17.49,21.19), height = 25, width = 25, colorA = 150 },
 		}
 	},
 	{
 		Trigger = "fog",
 		SubMaterialID = 25,
 		Sprites = {
-			[1] = {
-				pos = Vector(96.88,17.33,20.92),
-				colorB = 200,
-				colorA = 150,
-			},
-			[2] = {
-				pos = Vector(96.88,-17.33,20.92),
-				colorB = 200,
-				colorA = 150,
-			},
+			{ pos = Vector(96.88,17.33,20.92), colorB = 200, colorA = 150 },
+			{ pos = Vector(96.88,-17.33,20.92), colorB = 200, colorA = 150 },
 		},
 	},
 }
