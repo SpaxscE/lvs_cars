d13a90fbb44a20c88820123f4347057321497519
Diggergithub <88580941+Diggergithub@users.noreply.github.com>
2023-08-13 12:12:48 +0500

    dc2 added

---
+++ lua/entities/lvs_wheeldrive_dc2/init.lua
@@ -0,0 +1,102 @@
+AddCSLuaFile( "shared.lua" )
+AddCSLuaFile( "cl_init.lua" )
+include("shared.lua")
+
+function ENT:OnSpawn( PObj )
+	local DriverSeat = self:AddDriverSeat( Vector(-12,-15.2,2), Angle(0,-90,10) )
+	local PassengerSeat = self:AddPassengerSeat( Vector(-2,15.2,12), Angle(0,-90,20) )
+
+	self:AddEngine( Vector(45,0,20) )
+
+	self:AddEngine( Vector(45,0,20) )
+
+	local DoorHandler = self:AddDoorHandler( "left_door", Vector(0,31,26), Angle(0,0,0), Vector(-25,-6,-16), Vector(32,6,8), Vector(-25,-20,-16), Vector(32,42,8) )
+	DoorHandler:SetSoundOpen( "lvs/vehicles/generic/car_door_open.wav" )
+	DoorHandler:SetSoundClose( "lvs/vehicles/generic/car_door_close.wav" )
+	DoorHandler:LinkToSeat( PassengerSeat )
+
+	local DoorHandler = self:AddDoorHandler( "right_door", Vector(0,-31,26), Angle(0,0,0), Vector(-25,-6,-16), Vector(32,6,8), Vector(-25,-42,-16), Vector(32,20,8) )
+	DoorHandler:SetSoundOpen( "lvs/vehicles/generic/car_door_open.wav" )
+	DoorHandler:SetSoundClose( "lvs/vehicles/generic/car_door_close.wav" )
+	DoorHandler:LinkToSeat( DriverSeat )
+
+	local DoorHandler = self:AddDoorHandler( "trunk", Vector(-70,0,40), Angle(-8,0,0), Vector(-15,-25,-3), Vector(10,25,3), Vector(-15,-30,-3), Vector(10,30,40) )
+	DoorHandler:SetSoundOpen( "lvs/vehicles/generic/car_trunk_open.wav" )
+	DoorHandler:SetSoundClose( "lvs/vehicles/generic/classiccar_door_close.wav" )
+
+	local DoorHandler = self:AddDoorHandler( "hood", Vector(65,0,30), Angle(10,0,0), Vector(-30,-30,-3), Vector(20,30,3), Vector(-30,-30,-3), Vector(0,30,40) )
+	DoorHandler:SetSoundOpen( "lvs/vehicles/generic/car_hood_open.wav" )
+	DoorHandler:SetSoundClose( "lvs/vehicles/generic/car_hood_close.wav" )
+
+	local WheelModel = "models/DiggerCars/HONDA_INTEGRA_DC2/dc2_wheel.mdl"
+
+	local FrontAxle = self:DefineAxle( {
+		Axle = {
+			ForwardAngle = Angle(0,0,0),
+			SteerType = LVS.WHEEL_STEER_FRONT,
+			SteerAngle = 30,
+			TorqueFactor = 1,
+			BrakeFactor = 1,
+		},
+		Wheels = {
+			self:AddWheel( {
+				pos = Vector(52,28,14),
+				mdl = WheelModel,
+				mdl_ang = Angle(-90,0,-90),
+			} ),
+
+			self:AddWheel( {
+				pos = Vector(52,-28,14),
+				mdl = WheelModel,
+				mdl_ang = Angle(-90,180,-90),
+			} ),
+		},
+		Suspension = {
+			Height = 10,
+			MaxTravel = 7,
+			ControlArmLength = 25,
+			SpringConstant = 20000,
+			SpringDamping = 2000,
+			SpringRelativeDamping = 2000,
+		},
+	} )
+
+	local RearAxle = self:DefineAxle( {
+		Axle = {
+			ForwardAngle = Angle(0,0,0),
+			SteerType = LVS.WHEEL_STEER_NONE,
+			TorqueFactor = 0,
+			BrakeFactor = 1,
+			UseHandbrake = true,
+		},
+		Wheels = {
+			self:AddWheel( {
+				pos = Vector(-54,28,17),
+				mdl = WheelModel,
+				mdl_ang = Angle(-90,0,-90),
+			} ),
+
+			self:AddWheel( {
+				pos = Vector(-54,-28,17),
+				mdl = WheelModel,
+				mdl_ang = Angle(-90,180,-90),
+			} ),
+		},
+		Suspension = {
+			Height = 10,
+			MaxTravel = 7,
+			ControlArmLength = 25,
+			SpringConstant = 20000,
+			SpringDamping = 2000,
+			SpringRelativeDamping = 2000,
+		},
+	} )
+end
+
+function ENT:OnEngineActiveChanged( Active )
+	if Active then
+		self:EmitSound( "lvs/vehicles/kuebelwagen/engine_start.wav" )
+	else
+		self:EmitSound( "lvs/vehicles/kuebelwagen/engine_stop.wav" )
+	end
+end
---
+++ lua/entities/lvs_wheeldrive_dc2/shared.lua
@@ -0,0 +1,51 @@
+
+ENT.Base = "lvs_base_wheeldrive"
+
+ENT.PrintName = "Honda Integra"
+ENT.Author = "Digger"
+ENT.Information = "Luna's Vehicle Script"
+ENT.Category = "[LVS] - Cars"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+ENT.MDL = "models/DiggerCars/HONDA_INTEGRA_DC2/dc2.mdl"
+
+ENT.MaxVelocity = 2200
+
+ENT.EnginePower = 600
+ENT.EngineTorque = 235
+
+ENT.TransGears = 5
+ENT.TransGearsReverse = 1
+ENT.TransMinGearHoldTime = 1
+ENT.TransShiftSpeed = 0.3
+
+ENT.EngineSounds = {
+	{
+		sound = "lvs/vehicles/bmw_m5e34/eng_idle_loop.wav",
+		Volume = 1,
+		Pitch = 85,
+		PitchMul = 25,
+		SoundLevel = 75,
+		SoundType = LVS.SOUNDTYPE_IDLE_ONLY,
+	},
+	{
+		sound = "lvs/vehicles/bmw_m5e34/eng_loop.wav",
+		Volume = 1,
+		Pitch = 80,
+		PitchMul = 110,
+		SoundLevel = 75,
+		SoundType = LVS.SOUNDTYPE_REV_UP,
+		UseDoppler = true,
+	},
+	{
+		sound = "lvs/vehicles/bmw_m5e34/eng_revdown_loop.wav",
+		Volume = 1,
+		Pitch = 80,
+		PitchMul = 110,
+		SoundLevel = 75,
+		SoundType = LVS.SOUNDTYPE_REV_DOWN,
+		UseDoppler = true,
+	},
+}
