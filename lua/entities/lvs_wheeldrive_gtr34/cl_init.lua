03be9bebff887bab80c4f8e39b734c9164be5a72
Diggergithub <88580941+Diggergithub@users.noreply.github.com>
2023-08-16 12:14:34 +0500

    r34

---
+++ lua/entities/lvs_wheeldrive_gtr34/cl_init.lua
@@ -0,0 +1,23 @@
+include("shared.lua")
+
+function ENT:UpdatePoseParameters( steer, speed_kmh, engine_rpm, throttle, brake, handbrake, clutch, gear )
+	self:SetPoseParameter( "vehicle_steer", steer )
+	self:SetPoseParameter( "tacho_gauge", engine_rpm / 7000 )
+	self:SetPoseParameter( "vehicle_gauge", speed_kmh / 180 )
+	self:SetPoseParameter( "throttle_pedal", throttle )
+	self:SetPoseParameter( "brake_pedal", brake )
+	self:SetPoseParameter( "handbrake_pedal", handbrake )
+	self:SetPoseParameter( "clutch_pedal", clutch )
+
+	local GearIDtoPose = {
+		[-1] = 0,
+		[1] = 5,
+		[2] = 7,
+		[3] = 10,
+		[4] = 12,
+		[5] = 15,
+		[5] = 17,
+	}
+
+	self:SetPoseParameter( "gear",  self:QuickLerp( "gear", (GearIDtoPose[ gear ] or 1) ) )
+end
