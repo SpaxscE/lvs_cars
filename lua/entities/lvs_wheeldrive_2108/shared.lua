eecae20f6b1b0a5f3cc98465848c5c025b3f20c3
Luna <swagnemite@gmx.de>
2023-08-20 00:38:32 +0200

    this thing supposed to be so fast?

--- lua/entities/lvs_wheeldrive_2108/shared.lua
+++ lua/entities/lvs_wheeldrive_2108/shared.lua
@@ -14,7 +14,7 @@ ENT.MDL = "models/DiggerCars/VAZ 2108 Sport/v1.mdl"
 ENT.MaxVelocity = 2200
 
 ENT.EnginePower = 600
-ENT.EngineTorque = 235
+ENT.EngineTorque = 170
 
 ENT.TransGears = 5
 ENT.TransGearsReverse = 1
