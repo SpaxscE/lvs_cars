fc53b0f7f355e933dbd447256a95931d91ebecfb
Luna <swagnemite@gmx.de>
2023-08-20 15:29:57 +0200

    adjust dis

--- lua/entities/lvs_wheeldrive_city/cl_init.lua
+++ lua/entities/lvs_wheeldrive_city/cl_init.lua
@@ -2,7 +2,7 @@ include("shared.lua")
 
 function ENT:UpdatePoseParameters( steer, speed_kmh, engine_rpm, throttle, brake, handbrake, clutch, gear, temperature, fuel, oil, ammeter )
 	self:SetPoseParameter( "vehicle_steer", steer )
-	self:SetPoseParameter( "tacho_gauge", engine_rpm / 10000 )
+	self:SetPoseParameter( "tacho_gauge", engine_rpm / 7500 )
 	self:SetPoseParameter( "temp_gauge", temperature )
 	self:SetPoseParameter( "fuel_gauge", fuel )
 	self:SetPoseParameter( "throttle_pedal", throttle )
