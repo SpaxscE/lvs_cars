ec4434b5ea2ec9f8e2e6a1ad7314682044ea3a60
Luna <swagnemite@gmx.de>
2023-08-20 13:04:15 +0200

    a

--- lua/weapons/gmod_tool/stools/lvscareditor.lua
+++ lua/weapons/gmod_tool/stools/lvscareditor.lua
@@ -1,41 +1,21 @@
 
 TOOL.Category		= "LVS"
-TOOL.Name			= "#AI Enabler"
+TOOL.Name			= "#Car Editor"
 TOOL.Command		= nil
 TOOL.ConfigName		= ""
 
 if CLIENT then
-	language.Add( "tool.lvsaienabler.name", "AI Enabler" )
-	language.Add( "tool.lvsaienabler.desc", "A tool used to enable/disable AI on LVS-Vehicles" )
-	language.Add( "tool.lvsaienabler.0", "Left click on a LVS-Vehicle to enable AI, Right click to disable." )
-	language.Add( "tool.lvsaienabler.1", "Left click on a LVS-Vehicle to enable AI, Right click to disable." )
+	language.Add( "tool.lvscareditor.name", "Car Editor" )
+	language.Add( "tool.lvscareditor.desc", "A tool used to edit LVS-Cars" )
+	language.Add( "tool.lvscareditor.0", "Left click to cock." )
+	language.Add( "tool.lvscareditor.1", "Left click to cock." )
 end
 
 function TOOL:LeftClick( trace )
-	local ent = trace.Entity
-
-	if not IsValid( ent ) then return false end
-
-	if not ent.LVS and not ent.LFS then return end
-
-	if isfunction( ent.SetAI ) then
-		ent:SetAI( true )
-	end
-
 	return true
 end
 
 function TOOL:RightClick( trace )
-	local ent = trace.Entity
-
-	if not IsValid( ent ) then return false end
-
-	if not ent.LVS and not ent.LFS then return end
-
-	if isfunction( ent.SetAI ) then
-		ent:SetAI( false )
-	end
-
 	return true
 end
 
