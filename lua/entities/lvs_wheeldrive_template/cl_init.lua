345a6fa0ae11595c93ca7ea7a8b4653d41cef2b8
Luna <swagnemite@gmx.de>
2023-08-22 01:41:40 +0200

    Update cl_init.lua

--- lua/entities/lvs_wheeldrive_template/cl_init.lua
+++ lua/entities/lvs_wheeldrive_template/cl_init.lua
@@ -1,1 +1,20 @@
 include("shared.lua")
+
+function ENT:UpdatePoseParameters( steer, speed_kmh, engine_rpm, throttle, brake, handbrake, clutch, gear, temperature, fuel, oil, ammeter )
+	self:SetPoseParameter( "vehicle_steer", steer ) -- keep default behavior
+
+	--[[ add your gauges:
+
+	self:SetPoseParameter( "tacho_gauge", engine_rpm / 8000 )
+	self:SetPoseParameter( "temp_gauge", temperature )
+	self:SetPoseParameter( "fuel_gauge", fuel )
+	self:SetPoseParameter( "oil_gauge", oil )
+	self:SetPoseParameter( "alt_gauge", ammeter )
+	self:SetPoseParameter( "vehicle_gauge", speed_kmh / 240 )
+	self:SetPoseParameter( "throttle_pedal", throttle )
+	self:SetPoseParameter( "brake_pedal", brake )
+	self:SetPoseParameter( "handbrake_pedal", handbrake )
+	self:SetPoseParameter( "clutch_pedal", clutch )
+	]]
+	-- no need to call invalidatebonecache. Its called automatically after this function.
+end
