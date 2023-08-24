b9d8a8f0f61077c4816fc8269112e691bfa2192c
Diggergithub <88580941+Diggergithub@users.noreply.github.com>
2023-08-24 14:27:54 +0500

    quattro and highwayman updated, micra goodbye

--- lua/entities/lvs_wheeldrive_highwayman/init.lua
+++ lua/entities/lvs_wheeldrive_highwayman/init.lua
@@ -3,8 +3,26 @@ AddCSLuaFile( "cl_init.lua" )
 include("shared.lua")
 
 function ENT:OnSpawn( PObj )
-	self:AddDriverSeat( Vector(-10,16,-1), Angle(0,-90,10) )
-	self:AddPassengerSeat( Vector(0,-16,8), Angle(0,-90,20) )
+
+	local DriverSeat = self:AddDriverSeat( Vector(-10,16,-1), Angle(0,-90,10) )
+	local PassengerSeat = self:AddPassengerSeat( Vector(0,-16,8), Angle(0,-90,20) )
+	self:AddSuperCharger()
+
+	local DoorHandler = self:AddDoorHandler( "left_door", Vector(-5,35,22), Angle(0,0,0), Vector(-15,-6,-16), Vector(32,6,8), Vector(-15,-20,-16), Vector(32,42,8) )
+	DoorHandler:SetSoundOpen( "lvs/vehicles/generic/car_door_open.wav" )
+	DoorHandler:SetSoundClose( "lvs/vehicles/generic/car_door_close.wav" )
+	DoorHandler:LinkToSeat( DriverSeat )
+
+	local DoorHandler = self:AddDoorHandler( "right_door", Vector(-5,-35,22), Angle(0,0,0), Vector(-15,-6,-16), Vector(32,6,8), Vector(-15,-42,-16), Vector(32,20,8) )
+	DoorHandler:SetSoundOpen( "lvs/vehicles/generic/car_door_open.wav" )
+	DoorHandler:SetSoundClose( "lvs/vehicles/generic/car_door_close.wav" )
+	DoorHandler:LinkToSeat( PassengerSeat )
+
+	local DoorHandler = self:AddDoorHandler( "hood", Vector(65,0,28), Angle(5,0,0), Vector(-30,-27,-3), Vector(25,27,3), Vector(-30,-27,-3), Vector(0,27,40) )
+	DoorHandler:SetSoundOpen( "lvs/vehicles/generic/car_hood_open.wav" )
+	DoorHandler:SetSoundClose( "lvs/vehicles/generic/car_hood_close.wav" )
+
+
 
 	self:AddEngine( Vector(65,0,25) )
 
