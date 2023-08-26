38fa8f9c8aeff3e5e0cf075ac10fbc6c7598f1fe
Luna <swagnemite@gmx.de>
2023-08-26 23:10:02 +0200

    sherman armor

--- lua/entities/lvs_wheeldrive_dodsherman/init.lua
+++ lua/entities/lvs_wheeldrive_dodsherman/init.lua
@@ -33,7 +33,29 @@ function ENT:OnSpawn( PObj )
 	self:SetGunnerSeat( GunnerSeat )
 
 	self:AddEngine( Vector(-79.66,0,72.21) )
-	self:AddFuelTank( Vector(-80,0,20), Angle(-15,0,0), 600, LVS.FUELTYPE_PETROL, Vector(-10,-30,0),Vector(10,30,40) )
+	self:AddFuelTank( Vector(-75,0,20), Angle(0,0,0), 600, LVS.FUELTYPE_PETROL, Vector(-10,-30,0),Vector(10,30,40) )
 
 	self:CreateTracks()
+
+	-- front upper wedge
+	self:AddArmor( Vector(90,0,60), Angle(30,0,0), Vector(-40,-45,-15), Vector(25,45,-2), 1500, 150000 )
+
+	-- front lower wedge
+	self:AddArmor( Vector(105,0,30), Angle(-45,0,0), Vector(-10,-45,-15), Vector(15,45,10), 750, 150000 )
+
+	-- side armor
+	self:AddArmor( Vector(0,45,20), Angle(0,0,0), Vector(-100,-15,-20), Vector(120,15,55), 750, 150000 )
+	self:AddArmor( Vector(0,-45,20), Angle(0,0,0), Vector(-100,-15,-20), Vector(120,15,55), 750, 150000 )
+
+	-- roof
+	self:AddArmor( Vector(0,0,68), Angle(-3,0,0), Vector(-105,-45,0), Vector(55,45,10), 2000 )
+
+	-- turret
+	self:AddArmor( Vector(1.5,0,70), Angle(0,0,0), Vector(-50,-40,0), Vector(40,40,40), 2000, 150000 )
+
+	-- rear
+	self:AddArmor( Vector(-90,0,20), Angle(-15,0,0), Vector(-10,-30,-5),Vector(10,30,50), 250 )
+
+	-- driver viewport weakstop
+	self:AddDriverViewPort( Vector(90,21,58), Angle(0,0,0), Vector(-2,-7,-2), Vector(2,7,2) )
 end
