f2fb10ecdcaad843ae8fd071701ffcd30052260e
Luna <swagnemite@gmx.de>
2023-08-27 00:30:11 +0200

    fix halftrack steering animation

--- lua/entities/lvs_wheeldrive_dodhalftrack_us/cl_init.lua
+++ lua/entities/lvs_wheeldrive_dodhalftrack_us/cl_init.lua
@@ -4,6 +4,7 @@ include("sh_turret.lua")
 include("entities/lvs_tank_wheeldrive/cl_tankview.lua")
 
 function ENT:UpdatePoseParameters( steer, speed_kmh, engine_rpm, throttle, brake, handbrake, clutch, gear, temperature, fuel, oil, ammeter )
+	self:SetPoseParameter( "vehicle_steer", steer )
 	self:CalcTurret()
 end
 
