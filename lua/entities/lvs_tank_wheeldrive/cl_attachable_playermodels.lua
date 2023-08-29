a73773c266c3554c86318ee235f78c8cefaee8db
Luna <swagnemite@gmx.de>
2023-08-29 13:18:47 +0200

    remove unused code

--- lua/entities/lvs_tank_wheeldrive/cl_attachable_playermodels.lua
+++ lua/entities/lvs_tank_wheeldrive/cl_attachable_playermodels.lua
@@ -37,39 +37,6 @@ function ENT:CreatePlayerModel( ply, name )
 	return model
 end
 
-function ENT:DrawDriver()
-	local pod = self:GetDriverSeat()
-
-	if not IsValid( pod ) then self:RemovePlayerModel( "driver" ) return end
-
-	local plyL = LocalPlayer()
-	local ply = self:GetDriver()
-
-	if not IsValid( ply ) or (ply == plyL and not pod:GetThirdPersonMode()) then self:RemovePlayerModel( "driver" ) return end
-
-	local ID = self:LookupAttachment( "seat1" )
-	local Att = self:GetAttachment( ID )
-
-	if not Att then self:RemovePlayerModel() return end
-
-	local Pos,Ang = LocalToWorld( Vector(10,-5,0), Angle(0,20,-90), Att.Pos, Att.Ang )
-
-	local model = self:CreatePlayerModel( ply, "driver" )
-
-	if not IsValid( model ) then return end
-
-	model:SetSequence( "sit" )
-	model:SetRenderOrigin( Pos )
-	model:SetRenderAngles( Ang )
-	model:DrawModel()
-end
-
-function ENT:PreDraw()
-	self:DrawDriver()
-
-	return true
-end
-
 function ENT:OnRemoved()
 	self:RemovePlayerModel()
 end
