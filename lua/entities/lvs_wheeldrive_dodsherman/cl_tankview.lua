2422bf2d5b016ac67beba935a20b7e3705739418
Luna <swagnemite@gmx.de>
2023-09-16 17:34:02 +0200

    fix oppsi

--- lua/entities/lvs_wheeldrive_dodsherman/cl_tankview.lua
+++ lua/entities/lvs_wheeldrive_dodsherman/cl_tankview.lua
@@ -1,5 +1,5 @@
 
-include("entities/lvs_tank_wheeldrive/cl_tankview.lua")
+include("entities/lvs_tank_wheeldrive/modules/cl_tankview.lua")
 
 function ENT:TankViewOverride( ply, pos, angles, fov, pod )
 	if ply == self:GetDriver() and not pod:GetThirdPersonMode() then
