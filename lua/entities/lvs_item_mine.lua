f01702cb32bed73aa2d388ea94e59f416182b363
Luna <swagnemite@gmx.de>
2023-09-17 15:35:58 +0200

    allow repair tool to destroy mines

--- lua/entities/lvs_item_mine.lua
+++ lua/entities/lvs_item_mine.lua
@@ -47,7 +47,7 @@ if SERVER then
 	function ENT:Use( ply )
 	end
 
-	function ENT:Detonate()
+	function ENT:Detonate( nodamage )
 		if self.IsExploded then return end
 
 		self.IsExploded = true
@@ -61,6 +61,12 @@ if SERVER then
 			util.Effect( "WaterSurfaceExplosion", effectdata, true, true )
 		else
 			util.Effect( "lvs_defence_explosion", effectdata )
+		end
+
+		if nodamage then
+			self:Remove()
+
+			return
 		end
 
 		self:SetNoDraw( true )
