2bc7f603125366c2ffff3869b61b91daa1b71cd2
Luna <swagnemite@gmx.de>
2023-11-21 20:57:18 +0100

    Update shared.lua

--- lua/entities/lvs_trailer_flak/shared.lua
+++ lua/entities/lvs_trailer_flak/shared.lua
@@ -1,7 +1,7 @@
 
 ENT.Base = "lvs_base_wheeldrive_trailer"
 
-ENT.PrintName = "FlaK"
+ENT.PrintName = "FlaK 38"
 ENT.Author = "Luna"
 ENT.Information = "Luna's Vehicle Script"
 ENT.Category = "[LVS]"
