52ec3c170730b5da7f654722027a3d86a5479fcc
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-05-29 19:28:29 +0200

    bone poseparameter support for tracks

--- lua/entities/lvs_tank_wheeldrive/cl_init.lua
+++ lua/entities/lvs_tank_wheeldrive/cl_init.lua
@@ -68,7 +68,13 @@ function ENT:CalcTracks()
 
 		local RangeMul = data.PoseParameter.rangeMultiplier or 1
 
-		self:SetPoseParameter( data.PoseParameter.name, self:QuickLerp( data.PoseParameter.name, Dist * RangeMul, Rate ) )
+		if string.StartsWith( data.PoseParameter.name, "!" ) then
+
+			self:SetBonePoseParameter( data.PoseParameter.name, math.Clamp( self:QuickLerp( data.PoseParameter.name, Dist * RangeMul, Rate ) / data.PoseParameter.range, 0 , 1 ) )
+
+		else
+			self:SetPoseParameter( data.PoseParameter.name, self:QuickLerp( data.PoseParameter.name, Dist * RangeMul, Rate ) )
+		end
 	end
 
 	self:CalcTrackScrollTexture()
