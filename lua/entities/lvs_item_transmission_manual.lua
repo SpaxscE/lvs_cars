12b67ae0d9c9ce37ac98b8b738e2f6436b758513
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-06-27 12:07:50 +0200

    add items

---
+++ lua/entities/lvs_item_transmission_manual.lua
@@ -0,0 +1,52 @@
+AddCSLuaFile()
+
+ENT.Type            = "anim"
+
+ENT.PrintName = "Transmission - Manual"
+ENT.Author = "Luna"
+ENT.Category = "[LVS]"
+
+ENT.Spawnable		= true
+ENT.AdminOnly		= false
+
+if SERVER then
+	function ENT:SpawnFunction( ply, tr, ClassName )
+		if not tr.Hit then return end
+
+		local ent = ents.Create( ClassName )
+		ent:SetPos( tr.HitPos + tr.HitNormal * 5 )
+		ent:Spawn()
+		ent:Activate()
+
+		return ent
+	end
+
+	function ENT:Initialize()	
+		self:SetModel( "models/diggercars/manual.mdl" )
+		self:PhysicsInit( SOLID_VPHYSICS )
+		self:PhysWake()
+		self:SetCollisionGroup( COLLISION_GROUP_WEAPON )
+	end
+
+	function ENT:Think()
+		return false
+	end
+
+	function ENT:PhysicsCollide( data, physobj )
+	end
+
+	function ENT:OnTakeDamage( dmginfo )
+	end
+end
+
+if CLIENT then
+	function ENT:Draw( flags )
+		self:DrawModel( flags )
+	end
+
+	function ENT:OnRemove()
+	end
+
+	function ENT:Think()
+	end
+end
