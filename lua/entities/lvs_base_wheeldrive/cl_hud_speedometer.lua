03c93176b1ea82167651c14a0683d19462776f16
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-07-04 00:08:57 +0200

    add speedo gauge too

--- lua/entities/lvs_base_wheeldrive/cl_hud_speedometer.lua
+++ lua/entities/lvs_base_wheeldrive/cl_hud_speedometer.lua
@@ -1,4 +1,12 @@
-LVS:AddHudEditor( "Tach",  ScrW() - 310, ScrH() - 260,  300, 250, 300, 250, "TACH",
+LVS:AddHudEditor( "Speedo",  ScrW() * 0.5 - 300 - 25, ScrH() - 240,  300, 220, 300, 220, "SPEEDO",
+	function( self, vehicle, X, Y, W, H, ScrX, ScrY, ply )
+		if not vehicle.LVSHudPaintSpeedo then return end
+
+		vehicle:LVSHudPaintSpeedo( X, Y, W, H, ScrX, ScrY, ply )
+	end
+)
+
+LVS:AddHudEditor( "Tach",  ScrW() * 0.5 + 25, ScrH() - 240,  300, 220, 300, 220, "TACH",
 	function( self, vehicle, X, Y, W, H, ScrX, ScrY, ply )
 		if not vehicle.LVSHudPaintTach then return end
 
@@ -25,13 +33,18 @@ local THE_FONT = {
 }
 surface.CreateFont( "LVS_TACHOMETER", THE_FONT )
 
+THE_FONT.size = 50
+surface.CreateFont( "LVS_SPEEDOMETER", THE_FONT )
+
 local circles = include("includes/circles/circles.lua")
 
 local Center = 650
 
-local startAngle = 165
-local endAngle = 360
-local offsetAngle = 0
+local startAngleSpeedo = 180
+local endAngleSpeedo = 375
+
+local startAngleTach = 165
+local endAngleTach = 360
 
 local RingOuter = circles.New( CIRCLE_OUTLINED, 645, 0, 0, 35 )
 RingOuter:SetX( Center )
@@ -53,18 +66,113 @@ RingInnerRedline:SetX( Center )
 RingInnerRedline:SetY( Center )
 RingInnerRedline:SetMaterial( true )
 
+local VehicleSpeedo = {}
 local VehicleTach = {}
+
+function ENT:GetBakedSpeedoMaterial( MaxSpeed )
+	local Class = self:GetClass()
+
+	if VehicleSpeedo[ Class ] then return VehicleSpeedo[ Class ] end
+
+	local SpeedoRange = endAngleSpeedo - startAngleSpeedo
+
+	local Steps = math.ceil( MaxSpeed / 15 ) + 2
+	local AngleStep = SpeedoRange / Steps
+
+	local speedoRT = GetRenderTarget( "lvs_speedo_"..Class, Center * 2, Center * 2 )
+
+	local old = DisableClipping( true )
+
+	render.OverrideAlphaWriteEnable( true, true )
+
+	render.PushRenderTarget( speedoRT )
+
+	cam.Start2D()
+		render.ClearDepth()
+		render.Clear( 0, 0, 0, 0 )
+
+		surface.SetDrawColor( Color( 0, 0, 0, 200 ) )
+
+		RingOuter:SetStartAngle( startAngleSpeedo )
+		RingOuter:SetEndAngle( endAngleSpeedo )
+		RingOuter()
+
+		surface.SetDrawColor( color_white )
+
+		for i = 0, Steps do
+			local Ang = AngleStep * i + startAngleSpeedo
+
+			local AngX = math.cos( math.rad( Ang ) )
+			local AngY = math.sin( math.rad( Ang ) )
+
+			local StartX = Center + AngX * 554
+			local StartY = Center + AngY * 554
+
+			local EndX = Center + AngX * 635
+			local EndY = Center + AngY * 635
+
+
+			draw.NoTexture()
+			surface.DrawTexturedRectRotated( (StartX + EndX) * 0.5, (StartY + EndY) * 0.5, 90, 15, -Ang )
+
+			local TextX = Center - 3 + AngX * 500
+			local TextY = Center + AngY * 515
+
+			draw.SimpleText( i * 15, "LVS_SPEEDOMETER", TextX, TextY, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
+		end
+
+		for i = 1, Steps do
+			local Start = AngleStep * i + startAngleSpeedo
+
+			for n = 1, 5 do
+				local Ang = Start - (AngleStep / 6) * n
+
+				surface.SetDrawColor( Color(150,150,150,255) )
+
+				local AngX = math.cos( math.rad( Ang ) )
+				local AngY = math.sin( math.rad( Ang ) )
+
+				local StartX = Center + AngX * 575
+				local StartY = Center + AngY * 575
+
+				local EndX = Center + AngX * 635
+				local EndY = Center + AngY * 635
+
+				draw.NoTexture()
+				surface.DrawTexturedRectRotated( (StartX + EndX) * 0.5, (StartY + EndY) * 0.5, 60, 5, -Ang )
+			end
+		end
+
+		surface.SetDrawColor( color_white )
+
+		RingInner:SetStartAngle( startAngleSpeedo )
+		RingInner:SetEndAngle( endAngleSpeedo )
+		RingInner()
+	cam.End2D()
+
+	render.OverrideAlphaWriteEnable( false )
+
+	render.PopRenderTarget()
+
+	local Mat = CreateMaterial( "lvs_speedo_"..Class.."_mat", "UnlitGeneric", { ["$basetexture"] = speedoRT:GetName(), ["$translucent"] = 1, ["$vertexcolor"] = 1 } )
+
+	VehicleSpeedo[ Class ] = Mat
+
+	DisableClipping( old )
+
+	return Mat
+end
 
 function ENT:GetBakedTachMaterial( MaxRPM )
 	local Class = self:GetClass()
 
 	if VehicleTach[ Class ] then return VehicleTach[ Class ] end
 
-	local TachRange = endAngle - startAngle
+	local TachRange = endAngleTach - startAngleTach
 
 	local Steps = math.ceil(MaxRPM / 1000)
 	local AngleStep = TachRange / Steps
-	local AngleRedline = startAngle + (TachRange / MaxRPM) * self.EngineMaxRPM
+	local AngleRedline = startAngleTach + (TachRange / MaxRPM) * self.EngineMaxRPM
 
 	local tachRT = GetRenderTarget( "lvs_tach_"..Class, Center * 2, Center * 2 )
 
@@ -80,18 +188,18 @@ function ENT:GetBakedTachMaterial( MaxRPM )
 
 		surface.SetDrawColor( Color( 0, 0, 0, 200 ) )
 
-		RingOuter:SetStartAngle( startAngle - offsetAngle )
+		RingOuter:SetStartAngle( startAngleTach )
 		RingOuter:SetEndAngle( AngleRedline )
 		RingOuter()
 
 		RingOuterRedline:SetStartAngle( AngleRedline )
-		RingOuterRedline:SetEndAngle( endAngle + offsetAngle )
+		RingOuterRedline:SetEndAngle( endAngleTach )
 		RingOuterRedline()
 
 		surface.SetDrawColor( color_white )
 
 		for i = 0, Steps do
-			local Ang = AngleStep * i + startAngle
+			local Ang = AngleStep * i + startAngleTach
 
 			local AngX = math.cos( math.rad( Ang ) )
 			local AngY = math.sin( math.rad( Ang ) )
@@ -122,7 +230,7 @@ function ENT:GetBakedTachMaterial( MaxRPM )
 		end
 
 		for i = 1, Steps do
-			local Start = AngleStep * i + startAngle
+			local Start = AngleStep * i + startAngleTach
 
 			for n = 1, 9 do
 				local Ang = Start - (AngleStep / 10) * n
@@ -149,14 +257,14 @@ function ENT:GetBakedTachMaterial( MaxRPM )
 
 		surface.SetDrawColor( color_white )
 
-		RingInner:SetStartAngle( startAngle - offsetAngle )
+		RingInner:SetStartAngle( startAngleTach )
 		RingInner:SetEndAngle( AngleRedline )
 		RingInner()
 
 		surface.SetDrawColor( Color(255,0,0,255) )
 
 		RingInnerRedline:SetStartAngle( AngleRedline )
-		RingInnerRedline:SetEndAngle( endAngle + offsetAngle )
+		RingInnerRedline:SetEndAngle( endAngleTach )
 		RingInnerRedline()
 
 	cam.End2D()
@@ -176,10 +284,76 @@ end
 
 local TachNeedleColor = Color(255,0,0,255)
 local TachNeedleRadiusInner = 15
-local TachNeedleRadiusOuter = 140
+local TachNeedleRadiusOuter = 130
 local TachNeedleBlurTime = 0.1
 local TachNeedles = {}
 local CurRPM = 0
+local CurSpeed = 0
+
+function ENT:LVSHudPaintSpeedo( X, Y, w, h, ScrX, ScrY, ply )
+	if ply ~= self:GetDriver() then return end
+
+	if not self:GetRacingHud() then return end
+
+	local Delta = (self:GetVelocity():Length() * 0.09144 - CurSpeed) * RealFrameTime()
+
+	if Delta > 0 then
+		Delta = Delta * (self:IsViewPunchSuppressed() and 3 or 20)
+	else
+		Delta = Delta * 20
+	end
+
+	CurSpeed = CurSpeed + Delta
+
+	local MaxSpeed = math.ceil( math.max( self.MaxVelocity, self.MaxVelocityReverse ) * 0.09144,0)
+
+	surface.SetDrawColor( 255, 255, 255, 255 )
+	surface.SetMaterial( self:GetBakedSpeedoMaterial( MaxSpeed ) )
+	surface.DrawTexturedRect( X, Y, w, w )
+
+	local CenterX = X + w * 0.5
+	local CenterY = Y + w * 0.5
+
+	local T = CurTime()
+
+	local Ang = startAngleSpeedo + (endAngleSpeedo - startAngleSpeedo) * (CurSpeed / MaxSpeed)
+
+	local AngX = math.cos( math.rad( Ang ) )
+	local AngY = math.sin( math.rad( Ang ) )
+
+	if math.abs( Delta ) > 0 then
+		local data = {
+			StartX = (CenterX + AngX * TachNeedleRadiusInner),
+			StartY = (CenterY + AngY * TachNeedleRadiusInner),
+			EndX = (CenterX + AngX * TachNeedleRadiusOuter),
+			EndY = (CenterY + AngY * TachNeedleRadiusOuter),
+			Time = T + TachNeedleBlurTime
+		}
+
+		table.insert( TachNeedles, data )
+	else
+		local StartX = CenterX + AngX * TachNeedleRadiusInner
+		local StartY = CenterY + AngY * TachNeedleRadiusInner
+		local EndX = CenterX + AngX * TachNeedleRadiusOuter
+		local EndY = CenterY + AngY * TachNeedleRadiusOuter
+
+		surface.SetDrawColor( TachNeedleColor )
+		surface.DrawLine( StartX, StartY, EndX, EndY )
+	end
+
+	for index, data in pairs( TachNeedles ) do
+		if data.Time < T then
+			TachNeedles[ index ] = nil
+
+			continue
+		end
+
+		local Brightness = (data.Time - T) / TachNeedleBlurTime
+
+		surface.SetDrawColor( Color( TachNeedleColor.r * Brightness, TachNeedleColor.g * Brightness, TachNeedleColor.b * Brightness, TachNeedleColor.a * Brightness ^ 2 ) )
+		surface.DrawLine( data.StartX, data.StartY, data.EndX, data.EndY )
+	end
+end
 
 function ENT:LVSHudPaintTach( X, Y, w, h, ScrX, ScrY, ply )
 	if ply ~= self:GetDriver() then return end
@@ -205,7 +379,7 @@ function ENT:LVSHudPaintTach( X, Y, w, h, ScrX, ScrY, ply )
 
 	local T = CurTime()
 
-	local Ang = startAngle + (endAngle - startAngle) * (CurRPM / MaxRPM)
+	local Ang = startAngleTach + (endAngleTach - startAngleTach) * (CurRPM / MaxRPM)
 
 	local AngX = math.cos( math.rad( Ang ) )
 	local AngY = math.sin( math.rad( Ang ) )
