0475f7ad3a8d6e93bbaeb1c4cc5d5004d43d535e
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-07-25 17:49:48 +0200

    invert effects

--- lua/effects/lvs_carfueltank_fire.lua
+++ lua/effects/lvs_carfueltank_fire.lua
@@ -28,24 +28,6 @@ EFFECT.SmokeMat = {
 	[16] = Material( "particle/smokesprites_0016" ),
 }
 
-EFFECT.Smoke = {
-	"particle/smokesprites_0001",
-	"particle/smokesprites_0002",
-	"particle/smokesprites_0003",
-	"particle/smokesprites_0004",
-	"particle/smokesprites_0005",
-	"particle/smokesprites_0006",
-	"particle/smokesprites_0007",
-	"particle/smokesprites_0008",
-	"particle/smokesprites_0009",
-	"particle/smokesprites_0010",
-	"particle/smokesprites_0011",
-	"particle/smokesprites_0012",
-	"particle/smokesprites_0013",
-	"particle/smokesprites_0014",
-	"particle/smokesprites_0015",
-	"particle/smokesprites_0016"
-}
 
 function EFFECT:Init( data )
 	local Pos = data:GetOrigin()
@@ -56,40 +38,10 @@ function EFFECT:Init( data )
 
 	if not IsValid( Ent ) then return end
 
-	local randomPos = VectorRand() * 15
-	randomPos.z = 0
-
 	self.Ent = Ent
-	self.Pos = Ent:WorldToLocal( Pos + randomPos )
+	self.Pos = Ent:WorldToLocal( Pos + VectorRand() * 8 )
 	self.RandomSize = math.Rand( 0.8, 1.6 )
 	self.Vel = self.Ent:GetVelocity()
-
-	local emitter = Ent:GetParticleEmitter( self.Pos )
-
-	if not IsValid( emitter ) then return end
-
-	for i = 1, 8 do
-		local particle = emitter:Add( self.Smoke[ math.random(1, #self.Smoke ) ], Pos )
-
-		local Dir = Angle(0,math.Rand(-180,180),0):Forward()
-		Dir.z = -0.5
-		Dir:Normalize()
-
-		if particle then
-			particle:SetVelocity( Dir * 250 )
-			particle:SetDieTime( 0.5 + i * 0.01 )
-			particle:SetAirResistance( 125 ) 
-			particle:SetStartAlpha( 100 )
-			particle:SetStartSize( 20 )
-			particle:SetEndSize( 40 )
-			particle:SetRoll( math.Rand(-1,1) * math.pi )
-			particle:SetRollDelta( math.Rand(-1,1) * 3 )
-			particle:SetColor( 0, 0, 0 )
-			particle:SetGravity( Vector( 0, 0, 600 ) )
-			particle:SetCollide( true )
-			particle:SetBounce( 0 )
-		end
-	end
 end
 
 function EFFECT:Think()
@@ -105,7 +57,25 @@ end
 function EFFECT:Render()
 	if not IsValid( self.Ent ) or not self.Pos then return end
 
+	self:RenderSmoke()
 	self:RenderFire()
+end
+
+function EFFECT:RenderSmoke()
+	local Scale = (self.DieTime - CurTime()) / self.LifeTime
+
+	local Pos = self.Ent:LocalToWorld( self.Pos ) + Vector(0,0,25)
+
+	local InvScale = 1 - Scale
+
+	local Num = #self.SmokeMat - math.Clamp(math.ceil( Scale * #self.SmokeMat ) - 1,0, #self.SmokeMat - 1)
+
+	local C = 10 + 10 * self.RandomSize
+	local Size = (25 + 30 * InvScale) * self.RandomSize
+	local Offset = (self.Vel * InvScale ^ 2) * 0.5
+
+	render.SetMaterial( self.SmokeMat[ Num ] )
+	render.DrawSprite( Pos + Vector(0,0,InvScale ^ 2 * 80 * self.RandomSize) - Offset, Size, Size, Color( C, C, C, 200 * Scale) )
 end
 
 function EFFECT:RenderFire()
@@ -122,9 +92,9 @@ function EFFECT:RenderFire()
 
 	local Num = #self.FireMat - math.Clamp(math.ceil( Scale * #self.FireMat ) - 1,0, #self.FireMat - 1)
 
-	local Size = (5 + 20 * Scale) * self.RandomSize
+	local Size = (10 + 25 * Scale) * self.RandomSize
 	local Offset = (self.Vel * InvScale ^ 2) * 0.025
 
 	render.SetMaterial( self.FireMat[ Num ] )
-	render.DrawSprite( Pos + Vector(0,0,InvScale ^ 2 * 30) - Offset, Size, Size, Color( 255, 255, 255, 255) )
+	render.DrawSprite( Pos + Vector(0,0,InvScale ^ 2 * 25) - Offset, Size, Size, Color( 255, 255, 255, 255) )
 end
