3a1b63c7c3ccecc4920f8475636f28c49760a61b
Luna <24762027+SpaxscE@users.noreply.github.com>
2024-12-18 01:43:33 +0100

    Update sv_axle.lua

--- lua/entities/lvs_wheeldrive_wheel/sv_axle.lua
+++ lua/entities/lvs_wheeldrive_wheel/sv_axle.lua
@@ -36,7 +36,7 @@ function ENT:Destroy()
 
 	if not IsValid( Master ) or IsValid( self.bsLockDMG ) then return end
 
-	local Fric = 0.8
+	local Fric = 10
 
 	self.bsLockDMG = constraint.AdvBallsocket(self,Master,0,0,vector_origin,vector_origin,0,0,-180,-180,-180,180,180,180,Fric,Fric,Fric,1,1)
 	self.bsLockDMG.DoNotDuplicate = true
